850eb66c35d955198788094306b50a780c97fcbe
Grayson Flood <floodgrayson@Graysons-MacBook-Pro.local>
2020-05-07 21:42:11 -0400

    OP-22 created first utils test, test passing

---
+++ test/test.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import { toggleValidColor } from '../src/lib/adminUtils';
+
+describe('adminUtils', function() {
+  describe('#toggleValidColor', function() {
+    it('should return "b-is-not-valid" for a blank input with type 0', function(done) {
+      const ret = toggleValidColor('testcolor', 0);
+
+      expect(ret).to.be.a('string');
+      expect(ret).to.equal('b-is-not-valid');
+      done();
+    });
+  });
+});
